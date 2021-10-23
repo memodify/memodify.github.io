177d371830e28b804be768c8ae18e8da06619e85
Tomotaka Sakuma <ktmtmks@gmail.com>
2021-10-23 14:20:43 +0900

    new: support GA.

---
+++ lib/ga.ts
@@ -0,0 +1,57 @@
+import { useRouter } from "next/dist/client/router";
+import { useEffect } from "react";
+
+// https://qiita.com/Nishi53454367/items/b74c66c988539a7f8d4d
+const gtagId = `gtag${Date.now()}`;
+const gtagScriptId = `gtagscript${Date.now()}`;
+
+export function setupGA(gaId: string | undefined) {
+  document.getElementById(gtagScriptId)?.remove();
+  document.getElementById(gtagId)?.remove();
+  if (!gaId) {
+    return;
+  }
+
+  const loadGtag = document.createElement("script");
+  loadGtag.id = gtagId;
+  loadGtag.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+  loadGtag.async = true;
+  document.head.appendChild(loadGtag);
+
+  const initGtag = document.createElement("script");
+  initGtag.id = gtagScriptId;
+  initGtag.text = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', '${gaId}');
+  `;
+  document.head.appendChild(initGtag);
+}
+
+declare global {
+  interface Window {
+    gtag?: (
+      key: string,
+      trackingId: string,
+      config: { page_path: string }
+    ) => void;
+  }
+}
+
+export function useTracking(gaId: string | undefined) {
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on("routeChangeStart", (location) => {
+      if (!window.gtag) {
+        return;
+      }
+      if (!gaId) {
+        return;
+      }
+      window.gtag("config", gaId, {
+        page_path: location.pathname,
+      });
+    });
+  });
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
+import { useEffect } from "react";
+import { setupGA, useTracking } from "../lib/ga";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const id = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID;
+  useEffect(() => {
+    setupGA(id);
+  });
+  useTracking(id);
   return <Component {...pageProps} />;
 }
 export default MyApp;
