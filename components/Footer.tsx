33d2b892980b64b71cd2db58ca70bbde5ba8ac65
Tomotaka Sakuma <ktmtmks@gmail.com>
2022-04-09 10:08:06 +0900

    fix: year of copyright.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,7 +18,7 @@ export default function Footer() {
           <span className="ml-3 text-xl">Memodify</span>
         </a>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-          © 2021 Tomotaka Sakuma —
+          © 2022 Tomotaka Sakuma —
           <a
             href="https://twitter.com/tmtk75"
             className="text-gray-600 ml-1"
