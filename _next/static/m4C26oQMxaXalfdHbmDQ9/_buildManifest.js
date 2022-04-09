a1105db118abcf191169d8aa29a2d2c43d74bae8
tmtk75 <tmtk75@users.noreply.github.com>
2022-04-09 01:09:48 +0000

    deploy: 753af16de41ddeb112406e65333c033cd244ed29

---
+++ _next/static/m4C26oQMxaXalfdHbmDQ9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1a38d3a52c9f1672.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
