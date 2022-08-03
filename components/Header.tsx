8c3b595d0335b7336c92b72ae1931c5e9dc24759
Tomotaka Sakuma <ktmtmks@gmail.com>
2022-08-03 23:58:06 +0900

    add: a sample to set arbitrary font in tailwindcss.

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <Logo width={24} height={24} />
-          <span className="ml-3 text-xl">Memodify</span>
+          <span className="ml-3 text-xl font-['Inter var']">Memodify</span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
           <a className="mr-5 hover:text-gray-900" href="#feature">
