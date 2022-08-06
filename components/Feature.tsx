73f76a6b897b7d3add75f57f99dca1d9ba756973
Tomotaka Sakuma <ktmtmks@gmail.com>
2022-08-06 21:57:19 +0900

    fix: to camel case.

--- components/Feature.tsx
+++ components/Feature.tsx
@@ -235,8 +235,8 @@ const svgLight = (
     stroke-width="2"
   >
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
     />
   </svg>
@@ -269,8 +269,8 @@ const svgLightening = (
     stroke-width="2"
   >
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       d="M13 10V3L4 14h7v7l9-11h-7z"
     />
   </svg>
@@ -303,8 +303,8 @@ const svgTag = (
     stroke-width="2"
   >
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"
     />
   </svg>
@@ -314,8 +314,8 @@ const svgPulse = (
   <svg
     fill="none"
     stroke="currentColor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     stroke-width="2"
     className="sm:w-16 sm:h-16 w-10 h-10"
     viewBox="0 0 24 24"
@@ -328,8 +328,8 @@ const svgRightArrow = (
   <svg
     fill="none"
     stroke="currentColor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     stroke-width="2"
     className="w-4 h-4 ml-2"
     viewBox="0 0 24 24"
@@ -342,8 +342,8 @@ const svgScissors = (
   <svg
     fill="none"
     stroke="currentColor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     stroke-width="2"
     className="sm:w-16 sm:h-16 w-10 h-10"
     viewBox="0 0 24 24"
@@ -358,8 +358,8 @@ const svgPerson = (
   <svg
     fill="none"
     stroke="currentColor"
-    stroke-linecap="round"
-    stroke-linejoin="round"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     stroke-width="2"
     className="sm:w-16 sm:h-16 w-10 h-10"
     viewBox="0 0 24 24"
