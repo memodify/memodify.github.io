3bfc186c8b2f65622d7982637c85a5649b8d5692
Tomotaka Sakuma <ktmtmks@gmail.com>
2022-09-03 14:19:42 +0900

    fix: layout.

--- pages/docs.tsx
+++ pages/docs.tsx
@@ -5,14 +5,14 @@ const Docs: NextPage = () => {
   return (
     <>
       <Header />
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center text-gray-700">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row text-gray-700">
         <div className="w-1/5">
-          <ul>
+          <ul className="flex flex-col">
             <li>
               <a href="/docs#getting-started">Getting Started</a>
             </li>
             <li>
-              <a href="/docs#config">Config</a>
+              <a href="/docs#settings">Settings</a>
             </li>
             <li>
               <a href="/docs#advanced">Advanced</a>
@@ -25,10 +25,13 @@ const Docs: NextPage = () => {
               Getting Started
             </h1>
             <p>TBD</p>
+            {/* {Array.from(new Array(50), (e, i) => i).map((e) => (
+              <p>{e}</p>
+            ))} */}
           </section>
           <section className="pt-24">
-            <h1 id="config" className="text-4xl font-bold">
-              Config
+            <h1 id="settings" className="text-4xl font-bold">
+              Settings
             </h1>
             <p>TBD</p>
           </section>
