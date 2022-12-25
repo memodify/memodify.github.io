f9452da37dff752e2f131a0986794b6265ac46f6
Tomotaka Sakuma <ktmtmks@gmail.com>
2022-12-25 16:46:24 +0900

    new: trial: blog/[id].tsx

---
+++ pages/blog/[id].tsx
@@ -0,0 +1,55 @@
+import fs from "fs";
+import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import dynamic from "next/dynamic";
+
+const Page: NextPage<Props> = (props) => {
+  return (
+    <>
+      an entry
+      <Main {...props} />
+    </>
+  );
+};
+
+const Main: NextPage<Props> = (props) => {
+  const Content = dynamic(() => import(`../../posts/${props.filename}`));
+  console.log({ Content });
+  return (
+    <>
+      <section className="prose">
+        <Content />
+      </section>
+    </>
+  );
+};
+
+export default Page;
+
+type Props = {
+  posts: string[];
+  filename: string;
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = ["/blog/test"]; // getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  const path = params?.id as string;
+  const posts = fs.readdirSync("./posts").map((e) => e);
+  const it = posts.find((e) => e.match(new RegExp(`.*${path}.*`)));
+  if (!it) {
+    throw new Error(`not found. ${path} is missing. check your the posts.`);
+  }
+
+  return {
+    props: {
+      posts,
+      filename: it,
+    },
+  };
+};
