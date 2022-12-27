08c2cb5d125cd5e5a39f63ee3ffdda8a6ac8291d
Tomotaka Sakuma <ktmtmks@gmail.com>
2022-12-27 22:10:47 +0900

    add: sticky footer with tailwind. add: update an entry.

--- components/layouts/PageLayout.tsx
+++ components/layouts/PageLayout.tsx
@@ -1,15 +1,30 @@
+import { relative } from "path";
 import type { Frontmatter } from "remark-next-mdx-frontmatter";
+import Footer from "../Footer";
+import Header from "../Header";
 
 type PageLayoutProps = {
   children: JSX.Element;
   frontmatter: Frontmatter;
 };
 
+/*
+ * Stick footer technique comes from https://stackoverflow.com/a/72232241/3161724.
+ * Take a look at https://tailwindcss.com/docs/height also.
+ */
+
 export default function PageLayout({ children, frontmatter }: PageLayoutProps) {
   return (
-    <article className="prose">
-      {frontmatter.title}
-      {children}
-    </article>
+    <div className="min-h-screen">
+      <Header />
+      <main className="prose mx-auto">
+        <h1>{frontmatter.title}</h1>
+        <div className="flex justify-end">{frontmatter.created_at}</div>
+        <article>{children}</article>
+      </main>
+      <div className="sticky top-[100vh]">
+        <Footer />
+      </div>
+    </div>
   );
 }
