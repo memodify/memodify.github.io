7905401aeb417b0efa1d10c26c999e5646773484
Tomotaka Sakuma <ktmtmks@gmail.com>
2023-04-10 22:18:19 +0900

    Revert "fix: rm unused console.log"
    This reverts commit a0bbd0b17feab62fddb5f09d8724dc9ec575ad3d.

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -66,6 +66,7 @@ type Props = {
 const postsDirectory = "./posts";
 
 export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  console.log({ params });
   const posts = fs
     .readdirSync(postsDirectory)
     .sort()
