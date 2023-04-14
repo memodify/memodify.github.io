23f089f9b6a0dc6d184e3c34c745352d9b44b8b8
Tomotaka Sakuma <ktmtmks@gmail.com>
2023-04-14 22:47:44 +0900

    new: use radix-ui switch for dark-mode toggle button.

--- components/elements/DarkModeToggleButton.tsx
+++ components/elements/DarkModeToggleButton.tsx
@@ -1,54 +1,74 @@
 import { useEffect } from "react";
+import * as SwitchPrimitive from "@radix-ui/react-switch";
 
 // Thanks, https://tailwindcomponents.com/component/switch-to-darkmode
 export function DarkModeToggleButton() {
   useEffect(() => {
     setDarkMode();
   }, []);
+
   return (
-    <button
-      className="w-10 h-5 rounded-full bg-white flex items-center transition duration-300 focus:outline-none drop-shadow"
-      onClick={toggleDarkMode}
+    <SwitchPrimitive.Root
+      className="w-10 h-5
+                 rounded-full
+                 relative
+                 flex items-center
+                 focus:outline-none
+                 drop-shadow
+                 bg-white
+                 cursor-default"
+      onCheckedChange={toggleDarkMode}
     >
-      <div
-        id="switch-toggle"
+      <SwitchPrimitive.Thumb
         className="
-           w-6 h-6 relative rounded-full transition
-          bg-yellow-500 -translate-x-2 p-1 text-white
-          dark:bg-gray-700 dark:translate-x-full"
+            w-6 h-6
+            p-1
+            relative rounded-full
+            transition-transform
+                -translate-x-2
+            dark:translate-x-full
+            bg-yellow-500 text-white dark:bg-gray-700 "
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          className="dark:hidden"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-          />
-        </svg>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          className="hidden dark:block"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-          />
-        </svg>
-      </div>
-    </button>
+        {lightIcon}
+        {darkIcon}
+      </SwitchPrimitive.Thumb>
+    </SwitchPrimitive.Root>
   );
 }
+
+const lightIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    className="dark:hidden"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+    />
+  </svg>
+);
+
+const darkIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    className="hidden dark:block"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+    />
+  </svg>
+);
 
 function toggleDarkMode() {
   if (localStorage.theme === "dark") {
