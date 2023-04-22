3ce55a50f634b8196e036e1316b51835765ac304
Tomotaka Sakuma <ktmtmks@gmail.com>
2023-04-22 20:57:19 +0900

    add: adjust left-drawer for the landing page.

--- components/elements/LeftDrawer.tsx
+++ components/elements/LeftDrawer.tsx
@@ -1,16 +1,16 @@
-const iconXMark = (
+const iconChevronLeft = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     strokeWidth={1.5}
     stroke="currentColor"
-    className="w-6 h-6"
+    className="w-5 h-5"
   >
     <path
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M6 18L18 6M6 6l12 12"
+      d="M15.75 19.5L8.25 12l7.5-7.5"
     />
   </svg>
 );
@@ -40,12 +40,15 @@ export const LeftDrawer = ({ children }: { children: JSX.Element }) => (
            duration-200
            h-screen
            overflow-y-auto
+           shadow-lg
            "
     >
-      <div>
-        <label htmlFor="left-drawer-toggle-state" className="flex justify-end">
-          {iconXMark}
-        </label>
+      <div className="
+             h-16 flex
+             justify-end items-center
+             px-2
+             ">
+        <label htmlFor="left-drawer-toggle-state">{iconChevronLeft}</label>
       </div>
       {children}
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -27,25 +27,31 @@ const Home: NextPage = () => (
 export default Home;
 
 const HeadingLines = () => (
-  <ul className="p-4">
-    {(
-      [
-        ["/", "Home"],
-        ["/#whats-this", "What's this"],
-        ["/#feature", "Feature"],
-        ["/#live-demo", "Live Demo"],
-        ["/#pricing", "Pricing"],
-        ["/docs", "Docs"],
-        ["/blog/", "Blog"],
-      ] as const
-    ).map(([href, label]) => {
-      return (
-        <li className="pb-4" key={label}>
-          <Link href={href} onClick={closeLeftDrawer}>
-            {label}
-          </Link>
-        </li>
-      );
-    })}
-  </ul>
+  <>
+    <ul className="px-12">
+      {(
+        [
+          ["/", "Home"],
+          ["/#whats-this", "What's this"],
+          ["/#feature", "Feature"],
+          ["/#live-demo", "Live Demo"],
+          ["/#pricing", "Pricing"],
+          ["/docs", "Docs"],
+          ["/blog/", "Blog"],
+        ] as const
+      ).map(([href, label]) => {
+        return (
+          <li
+            className="
+          [&:not(:nth-of-type(1))]:pt-4"
+            key={label}
+          >
+            <Link href={href} onClick={closeLeftDrawer}>
+              {label}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  </>
 );
