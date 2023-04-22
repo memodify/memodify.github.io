a39aae9a1388dd4dc55031ba350c88d6ca967939
Tomotaka Sakuma <ktmtmks@gmail.com>
2023-04-22 21:01:56 +0900

    refactor: move up a style.

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,31 +27,30 @@ const Home: NextPage = () => (
 export default Home;
 
 const HeadingLines = () => (
-  <>
-    <ul className="px-12">
-      {(
-        [
-          ["/", "Home"],
-          ["/#whats-this", "What's this"],
-          ["/#feature", "Feature"],
-          ["/#live-demo", "Live Demo"],
-          ["/#pricing", "Pricing"],
-          ["/docs", "Docs"],
-          ["/blog/", "Blog"],
-        ] as const
-      ).map(([href, label]) => {
-        return (
-          <li
-            className="
-          [&:not(:nth-of-type(1))]:pt-4"
-            key={label}
-          >
-            <Link href={href} onClick={closeLeftDrawer}>
-              {label}
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
-  </>
+  <ul
+    className="
+      px-12
+      [&>li:not(:nth-of-type(1))]:pt-4
+      "
+  >
+    {(
+      [
+        ["/", "Home"],
+        ["/#whats-this", "What's this"],
+        ["/#feature", "Feature"],
+        ["/#live-demo", "Live Demo"],
+        ["/#pricing", "Pricing"],
+        ["/docs", "Docs"],
+        ["/blog/", "Blog"],
+      ] as const
+    ).map(([href, label]) => {
+      return (
+        <li key={label}>
+          <Link href={href} onClick={closeLeftDrawer}>
+            {label}
+          </Link>
+        </li>
+      );
+    })}
+  </ul>
 );
