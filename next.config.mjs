c81a67ae99025723b319c0ee9c40f32a483f884b
Tomotaka Sakuma <ktmtmks@gmail.com>
2024-04-26 22:42:12 +0900

    fix: as next-v14.

--- next.config.mjs
+++ next.config.mjs
@@ -36,6 +36,8 @@ const nextConfig = {
     disableStaticImages: true,
   },
 
+  output: "export",
+
   webpack: (config) => {
     config.module.rules.push({
       // ref: https://zenn.dev/catnose99/articles/49c12f84182bdf
